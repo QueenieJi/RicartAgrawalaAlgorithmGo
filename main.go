d024f80fc5e2a013a5103c96e980af2cb9c3654d
Queenie <jiyuqi0310@gmail.com>
2020-07-30 15:00:28 +1000

    implement main process

--- main.go
+++ main.go
@@ -8,16 +8,17 @@ import (
 )
 
 // Global variables
-const NODENUM = 10
-
+const NODENUM := 2
+var globalMap
+var globalWG sync.WaitGroup
 // Use a node structure to save information
 type Node struct {
 	myNum        int
 	highestNum   int
 	replyTracker map[int]Node*   
 	requestCS    bool
-	deffered     map[int]Node*
-
+	defferedNode map[int]Node*
+	nodeChannel  chan Message
 }
 
 type Message struct {
@@ -27,12 +28,13 @@ type Message struct {
 }
 // make a new node
 func newNode(id int) Node* {
-
+	n := Node{id, 0, nil, false, map[int]*Node{}, make(chan Message)}
+	return &n
 }
 
 // send a message
 func (n Node*) sendMessage (msg Message) {
-
+	receiver := 
 }
 
 // receive a message
@@ -42,7 +44,19 @@ func (n Node*) receiveMessage(msg Message) {
 
 // main process
 func (n Node*) mainProcess () {
-
+	for {
+		fmt.Println("Node %d enters non-critical section ", n.id)
+		n.requestCS := true
+		n.myNum := highestNum + 1
+		// send messages to all other nodes
+		for i := 0; i < NODENUM; i++ {
+			if i == n.id{
+				continue
+			}
+			msg := Message{"request", n.id, globalMap[i]}
+			go n.sendMessage(msg)
+		}
+	}
 }
 
 // receive process
@@ -52,6 +66,7 @@ func (n Node*) receiveProcess() {}
 // main process
 func main () {
 	globalMap := map[int]*Node{}
+	globalWG.Add(NODENUM)
 	fmt.Printf("Create Node now ...\n");
 	for i := 0; i < NODENUM; i++ {
 		new := newNode(i)
@@ -64,5 +79,6 @@ func main () {
 		go globalMap[i].receiveProcess()
 		fmt.Println("Create main and receive process for node %d", i)
 	}
+	globalWG.wait()
 	fmt.Println("All Nodes have entered entered and exited the Critical Section\nEnding programme now.\n")
 } 
